b7072b2a5f705b110e1496f4a010227fd4a417c7
sumitkumams <107923889+sumitkumams@users.noreply.github.com>
2025-03-18 00:37:36 +0530

    fix changes for mysql

--- Functions.Templates/Templates-v2/MySqlTrigger-Python/blueprint_body.py
+++ Functions.Templates/Templates-v2/MySqlTrigger-Python/blueprint_body.py
@@ -1,5 +1,5 @@
 
-@$(BLUEPRINT_FILENAME).cosmos_db_trigger(arg_name="azcosmosdb", container_name="$(CONTAINER_NAME_INPUT)",
-                               database_name="$(DB_NAME_INPUT)", connection="$(CONNECTION_STRING_INPUT)") 
-def $(FUNCTION_NAME_INPUT)(azcosmosdb: func.DocumentList):
-    logging.info('Python CosmosDB triggered')
+@$(BLUEPRINT_FILENAME).cosmos_db_trigger(arg_name="azmysqlchangeslist", table_name="$(TABLE_NAME_INPUT)",
+                               connection="$(CONNECTION_STRING_INPUT)") 
+def $(FUNCTION_NAME_INPUT)(azmysqlchangeslist: str):
+    logging.info("MySQL Changes: %s", json.loads(azmysqlchangeslist))
